f1bb226863ee6a445e54179e7f9da8345afe5948
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-05-21 19:33:48 +0200

    hw 09, part 1/3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const getRandomHexColor = function () {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
 
+let intervalId = null;
+
+const WHITE_BODY_COLOR = '#ffffff';
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  if (!refs.btnStart.disabled) {
+    intervalId = setInterval(
+      () => (refs.body.style.backgroundColor = getRandomHexColor()),
+      1000
+    );
+  }
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function onBtnStopClick() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+
+  clearInterval(intervalId);
+}
